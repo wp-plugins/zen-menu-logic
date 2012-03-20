65c9221abc34a66b65f02fd07335346c2734c798
zenofwordpress <zenofwordpress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-20 01:59:27 +0000

    git-svn-id: https://plugins.svn.wordpress.org/zen-menu-logic/trunk@521201 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -12,12 +12,11 @@ Zen Menu Logic allows the user to select any of several custom menus to appear o
 == Description ==
 
 This plugin only works under the following conditions:
-1. the theme supports custom menus
-2. the theme has registered at least one menu location as Primary
-3. user has created at least one custom menu with one or more
-   menu items and designated it as being in the primary location.
+1. the theme has registered at least one menu location
+2. user has created at least one custom menu with one or more
+   menu items.
 
-If those 3 conditions are met, then the edit page for every page and post
+If those 2 conditions are met, then the edit page for every page and post
 and custom post type will contain a meta box listing the custom menus with
 radio buttons. All you need do is select which custom menu should display
 when that page is called.
@@ -27,17 +26,31 @@ when that page is called.
 1. Unzip the download package
 2. Upload `zen-menu-logic` to the `/wp-content/plugins/` directory
 3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Go to the Zen Menu Logic options page in the Setting's menu
+   and select which menu location is the one that the plugin will
+   work on.
 
 alternatively
 
 1. upload the zip file from the Admin plugins page
 2. then activate
+3. Go to the Zen Menu Logic options page in the Setting's menu
+   and select which menu location is the one that the plugin will
+   work on.
 
 == Frequently Asked Questions ==
 
 == Screenshots ==
 
 == Changelog ==
+
+= 1.2 =
+* There is a design flaw in v1.1 where I assumed that the name of the primary menu location
+* was primary.  That is the way it is in the 2011 theme.  But not all themes are like that.
+* So this new version has an options page as a menu item of the Settings Menu that lists all
+* the menu locations by name.  And you need to select which one is the primary, or, in other
+* words, which menu location the menu logic will work on.  Please let me know if there are
+* any questions.
 
 = 1.1 =
 * simplify code that tests is this plugin is supported by the theme
--- zen-menu-logic.php
+++ zen-menu-logic.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Zen Menu Logic
  * Plugin Uri: http://www.zenofwp.com/zen-plugins/zen-menu-logic/
- * Version: v1.1
+ * Version: v1.2
  * Author: <a href="http://www.zenofwp.com">Greg Turner</a>
  * Description: Allows user to denote on any page which of many custom menus is to be used as the primary menu.
  * License: GPLv2 or later
@@ -32,65 +32,109 @@ if (version_compare ($wp_version, "3.3.1", "<")) {
 
 if (!class_exists ('ZenOfWPMenuLogic')) {
 	class ZenOfWPMenuLogic {
-	
-		protected $primaryMenu;
 		
 		function ZenOfWPMenuLogic () {
 			if ($this->is_menulogic_supported())
 				if (is_admin ()) {
-					add_action ('save_post', array (&$this, 'save_menulogic'));
-					add_action ('add_meta_boxes', array (&$this, 'register_menulogic_metabox'));
+					add_action ('admin_menu', array (&$this, 'register_settings_menu'));
+					add_action ('admin_init', array (&$this, 'init_settings'));				
+				
+					if ($this->is_primary_set ()) {
+						add_action ('save_post', array (&$this, 'save_menulogic'));
+						add_action ('add_meta_boxes', array (&$this, 'register_menulogic_metabox'));
+					}
+
 				}
-				else
-					add_filter ('wp_nav_menu_args', array (&$this, 'menulogic'));
+				else {
+					if ($this->is_primary_set ())
+						add_filter ('wp_nav_menu_args', array (&$this, 'menulogic'));
+				}
+		}
+		
+		function register_settings_menu () {
+			add_options_page ('Zen Menu Logic Settings', 'Zen Menu Logic', 'manage_options', __FILE__, array (&$this, 'draw_options_page'));
+		}
+		
+		function init_settings () {
+			register_setting ('menu_logic_options', 'menu_logic_options', array (&$this, 'validate_menu_logic_options'));
+			add_settings_section ('zen_menu_logic_settings', 'Zen Menu Logic Settings', array (&$this, 'draw_expl'), __FILE__);
+			add_settings_field ('zen_menu_logic_settings_name', 'Select Primary Location', array (&$this, 'draw_option'), __FILE__, 'zen_menu_logic_settings');
+		}
+		
+		function draw_options_page () {
+		?>
+		<div class="wrap">
+		<?php screen_icon(); ?>
+		<h2>Zen Menu Logic</h2>
+		<form action="options.php" method="post">
+		<?php settings_fields ('menu_logic_options'); ?>
+		<?php do_settings_sections (__FILE__); ?>
+		<input name="Submit" type="submit" value="Save Changes"/>
+		</form></div>
+		<?php
+		}
+		
+		function draw_expl () {
+			echo 'Your theme may have registered multiple menu locations, under different names.<br/>';
+			echo 'There is a radio button for each named location.  Select which location you want<br/>';
+			echo 'the Zen Menu Logic to work on.';
+		}
+		
+		function draw_option () {
+			$locations = get_theme_mod('nav_menu_locations');
+			$keys = array_keys ($locations);
+			$options = get_option ('menu_logic_options');
+			if (!empty ($options))
+				if (isset ($options ['primary_name'])) {
+					$name = $options ['primary_name'];
+					echo '<ul>';	
+					foreach ($keys as $key) {
+						if ($key == $name)
+							echo '<li><input type="radio" name="menu_logic_options[primary_name]" id="zenofwp_menulogic_optioinselect" value="'.$key.'" checked/><label for="">'.$key.'</label></li>';
+						else
+							echo '<li><input type="radio" name="menu_logic_options[primary_name]" id="zenofwp_menulogic_optionselect" value="'.$key.'"/><label for="">'.$key.'</label></li>';
+					}	
+					echo '</ul>';	
+					return;
+				}
+			echo '<ul>';		
+			foreach ($keys as $key) {
+				echo '<li><input type="radio" name="menu_logic_options[primary_name]" id="zenofwp_menulogic_optionselect" value="'.$key.'"/><label for="">'.$key.'</label></li>';			
+			}
+			echo '</ul>';
+		}
+		
+		function validate_menu_logic_options ($input) {
+			return $input;
+		}
+		
+		function is_primary_set () {
+			$options = get_option ('menu_logic_options');
+			if (!empty ($options))
+				if (isset ($options ['primary_name']))
+					return true;
+			return false;
 		}
 		
 		function is_menulogic_supported () {	
-			// do we have a menu with primary location?
+			// do we have a least one menu location?
 			$locations = get_theme_mod('nav_menu_locations');
 			if (empty ($locations)) 		
 				return false;
-			if (!isset ($locations['primary'])) 	
-				return false;
-			if ($locations['primary'] == '') 			
-				return false;
-			if ($locations['primary'] == 0) 			
-				return false;
-			
-			// save the menu id that is in the primary location
-			$this->primaryMenu = $locations['primary'];	
 			
 			// now make sure there is at least one menu, that
-			// has at least one menu itme, whose id is the
-			// same as that of the primary location
+			// has at least one menu item
 			$nav_menus = wp_get_nav_menus( array('orderby' => 'name') );
 			if (empty ($nav_menus))
 				return false;
 			foreach ($nav_menus as $menu) {
 				if (isset ($menu->count))
 					if (0 < $menu->count)
-						if ($menu->term_id == $this->primaryMenu)
-							return true;
+						return true;
 			}
 			return false;			
 		}
-		
-		// return list of menu names and ids that have at least one
-		// menu item
-		function getMenuList () {
-			$i = 0;
-			$nav_menus = wp_get_nav_menus( array('orderby' => 'name') );
-			foreach ($nav_menus as $menu) 
-				if (isset ($menu->count))
-					if (0 < $menu->count) {
-						$x['id'] = $menu->term_id;
-						$x['name'] = $menu->name;
-						$retarray[$i] = $x;
-						$i++;
-					}
-			return $retarray;
-		}
-		
+			
 		// this function is called when user clicks update of a page or post
 		// this saves the menu choice for this page
 		function save_menulogic ($post_id) {
@@ -112,19 +156,35 @@ if (!class_exists ('ZenOfWPMenuLogic')) {
 			if ($menuId <= 0)
 				return;	  
 			update_post_meta ($post_id,'zenofwp_menulogic_menuselect', $menuId);
+		}	
+
+		// return list of menu names and ids that have at least one
+		// menu item
+		function getMenuList () {
+			$i = 0;
+			$nav_menus = wp_get_nav_menus( array('orderby' => 'name') );
+			foreach ($nav_menus as $menu) 
+				if (isset ($menu->count))
+					if (0 < $menu->count) {
+						$x['id'] = $menu->term_id;
+						$x['name'] = $menu->name;
+						$retarray[$i] = $x;
+						$i++;
+					}
+			return $retarray;
 		}		
 		
 		// get the menu choice for this page.  first see if it has been set
-		// and if so, does it match one of the menu id.  If so, return that,
-		// otherwise return the primary menu.
+		// and if so, does it match one of the menu id.  If so, return that
 		function getPageMenu ($id,$menus) {
 			$myMenu = get_post_meta ($id, 'zenofwp_menulogic_menuselect', true);
-			if (!empty ($myMenu))
+			if (isset ($myMenu))
 				foreach ($menus as $menu) {
 					if ($menu['id'] == $myMenu)
 						return $myMenu;				
 				}
-			return $this->primaryMenu;
+			update_post_meta ($id,'zenofwp_menulogic_menuselect', 0);				
+			return 0;
 		}
 
 		// displays the meta box
@@ -182,16 +242,21 @@ if (!class_exists ('ZenOfWPMenuLogic')) {
 		// menu be the one to display.
 		function menulogic ($args) {
 			global $post;
-			$id = $post->ID;		
-			$menuId = get_post_meta ($id, 'zenofwp_menulogic_menuselect', true);
-			if (isset ($menuId))
-				if (0 < $menuId)
-					if (!empty ($args))
-						if (isset ($args['theme_location']))
-							if ($args['theme_location'] == 'primary') {
-								$args['theme_location'] = '';
-								$args['menu'] = $menuId;
-							}
+			$id = $post->ID;				
+			$options = get_option ('menu_logic_options');
+			if (!empty ($options))
+				if (isset ($options ['primary_name'])) {
+					$name = $options ['primary_name'];		
+					$menuId = get_post_meta ($id, 'zenofwp_menulogic_menuselect', true);			
+					if (isset ($menuId))
+						if (0 < $menuId)
+							if (!empty ($args))
+								if (isset ($args['theme_location']))
+									if ($args['theme_location'] == $name) {
+										$args['theme_location'] = '';
+										$args['menu'] = $menuId;
+									}
+					}
 			return $args;	
 		}
 	
